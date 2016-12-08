6a16972bc2b99cfe7b93b83df27869cbd0ecc2fb
César Alberca <cesalberca@gmail.com>
2016-12-08 18:08:59 +0100

    Scroll to top

--- js/index.js
+++ js/index.js
@@ -1,10 +1,39 @@
 (function () {
-    function init () {
-        let iframes = document.querySelectorAll('iframe')
-        let aIframes = Array.from(iframes)
+    function scrollTo(element, to, duration) {
+        let start = element.scrollTop
+        , change = to - start
+        , currentTime = 0
+        , increment = 20
+        
+        const animateScroll = function () {        
+            currentTime += increment
+            let val = Math.easeInOutQuad(currentTime, start, change, duration)
+            element.scrollTop = val
 
-        aIframes.forEach(function (iframe) {
-            iframe.sandbox = ''
+            if (currentTime < duration) {
+                setTimeout(animateScroll, increment)
+            }
+        }
+
+        animateScroll()
+    } 
+
+    // http://robertpenner.com/easing/
+    Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d/2
+        if (t < 1) return c/2*t*t + b
+        t--
+        return -c/2 * (t*(t-2) - 1) + b
+    }
+
+    function init () {
+        let innerLinks = document.querySelectorAll('[data-scroll-top]')
+        let elementToScrollTo = document.body
+        
+        innerLinks.forEach(function (link) {
+            link.addEventListener('click', function () {
+                scrollTo(elementToScrollTo, 0, 500)
+            })
         })
     }
 
